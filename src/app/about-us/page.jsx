555ae8af11942c472d88bd57cb0bf45a40155918
Shoeb Mohiuddin <shoebm891@gmail.com>
2024-07-26 04:29:01 +0530

    first commit

---
+++ src/app/about-us/page.jsx
@@ -0,0 +1,98 @@
+export default function ABoutUs() {
+    return (
+      <>
+        <div className="flex flex-col w-full overflow-x-hidden font-sans font-thin">
+          <div className="flex flex-col-reverse sm:flex-row-reverse items-center justify-between sm:justify-between flex-col pl-0 pr-0">
+            <div className="flex flex-row justify-center mt-12 ml-8 mr-8 sm:ml-64 sm:mr-60">
+              <img className="img3 w-88 sm:pb-32" src="/logo.png" alt="labs1" />
+              <img className="img3 w-88 sm:pb-32" src="/logo.png" alt="labs2" />
+              <img className="img3 w-88 sm:pb-32" src="/logo.png" alt="labs3" />
+            </div>
+            <div className="flex flex-col ml-8 mr-8 sm:w-auto sm:pl-60 pr-0 sm:pt-24 sm:pt-0">
+              <div className="text-3xl font-extrabold">What is SDSClub?</div>
+              <div className="sm:w-180 pt-10 text-xl sm:text-lg sm:font-medium text-gray-500 leading-7">
+                SDSClub or Software Development Section Club is a student-run technical group, which aims to encourage the development of technology and innovation in the campus and beyond. We believe in learning and sharing knowledge; collaboration and experimentation; and human interaction that matters the most.
+              </div>
+            </div>
+          </div>
+          {/* <div className="flex flex-col-reverse mt-36 sm:flex-row sm:justify-between sm:ml-64 sm:mr-36 sm:pt-36">
+            <div className="mt-16 sm:mt-0 ml-8 mr-8">
+            </div>
+            <div className="flex flex-col mr-8 ml-8 sm:w-auto sm:ml-100 sm:pt-8">
+              <div className="text-3xl font-extrabold">What we do</div>
+              <div className="sm:w-140 pt-10 text-xl sm:text-lg sm:font-medium text-gray-500 leading-7">
+                We are involved in a lot of activities on the campus, off the campus and on the web. We conduct lectures and workshops on different fields of tech, organize hackathons, and also conduct online competitions on our various self developed applications.
+              </div>
+            </div>
+          </div> */}
+          {/* <div className="flex flex-col mt-32 sm:mt-100 w-full">
+            <div className="area_of_interest">
+              <div className="flex flex-col-reverse sm:flex-row-reverse items-center sm:justify-between">
+                <div className="sm:mr-28 sm:w-220 w-full mt-16 sm:flex sm:flex-row">
+                  <div>
+                    <div className="flex rounded shadow-sister flex-row ml-8 mr-8 h-auto sm:w-92">
+                      <div className="my-8 mx-10 w-20 h-20 sm:m-8 sm:w-auto sm:h-auto">
+                        <img src="/assets/images/interest1.png" alt="interest1" />
+                      </div>
+                      <div className="font-sans font-semibold self-center text-xl leading-7 sm:text-base align-middle sm:leading-7">
+                        Software Development
+                      </div>
+                    </div>
+                    <div className="mt-8">
+                      <div className="flex rounded shadow-sister mt-0 mr-0 flex-row ml-8 mr-8 sm:w-92 h-auto">
+                        <div className="my-8 mx-10 w-20 h-20 sm:m-8 sm:w-auto sm:h-auto">
+                          <img src="/assets/images/interest3.png" alt="interest3" />
+                        </div>
+                        <div className="font-sans font-semibold self-center text-xl leading-7 sm:text-base align-middle sm:leading-7">
+                          Product Design
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-8">
+                      <div className="flex rounded shadow-sister mt-0 mr-0 flex-row ml-8 mr-8 sm:w-92 h-auto">
+                        <div className="my-8 mx-10 w-20 h-20 sm:m-8 sm:w-auto sm:h-auto">
+                          <img src="/assets/images/interest5.png" alt="interest5" />
+                        </div>
+                        <div className="font-sans font-semibold self-center text-xl leading-7 sm:text-base align-middle sm:leading-7 sm:mr-10">
+                          Game Development
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                  <div className="sm:-ml-8 sm:mt-12">
+                    <div className="mt-8">
+                      <div className="flex rounded shadow-sister mt-0 flex-row ml-8 mr-8 sm:w-92 h-auto">
+                        <div className="my-8 mx-10 w-20 h-20 sm:m-8 sm:w-auto sm:h-auto">
+                          <img src="/assets/images/interest2.png" alt="interest2" />
+                        </div>
+                        <div className="font-sans font-semibold self-center text-xl leading-7 sm:text-base align-middle sm:leading-7 sm:mr-10">
+                          Information Security
+                        </div>
+                      </div>
+                    </div>
+                    <div className="mt-8">
+                      <div className="flex rounded shadow-sister mt-0 mr-0 flex-row ml-8 mr-8 sm:w-92 h-auto">
+                        <div className="my-8 mx-10 w-20 h-20 sm:m-8 sm:w-auto sm:h-auto">
+                          <img src="/assets/images/interest4.png" alt="interest4" />
+                        </div>
+                        <div className="font-sans font-semibold self-center text-xl leading-7 sm:text-base align-middle sm:leading-7">
+                          Data Science
+                        </div>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+                <div className="flex flex-col sm:pl-60 mx-10">
+                  <div className="text-3xl font-extrabold">Areas of Interest</div>
+                  <div className="sm:w-180 pt-10 text-xl sm:text-lg sm:font-medium text-gray-500 leading-7">
+                    We constantly explore and experiment, try to learn new things, and get out of the comfort zones. This gives the members of SDSClub a freedom to work in a diverse range of fields in technology and innovation, some of which are mentioned here.
+                  </div>
+                </div>
+              </div>
+            </div>
+  
+          </div> */}
+        </div>
+      </>
+    );
+  }
---
+++ src/app/components/navigation/Navbar.jsx
@@ -0,0 +1,58 @@
+"use client"
+import { useState } from 'react';
+import Link from 'next/link';
+import { IoMdMenu, IoMdClose } from 'react-icons/io';
+
+const Navbar = () => {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const onToggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  return (
+    <header className="bg-white">
+      <nav className="flex justify-between items-center w-[92%] mx-auto">
+        <div>
+          <img
+            className="w-28 cursor-pointer"
+            src="logo.png"
+            alt="Logo"
+            href="#"
+          />
+        </div>
+        <div
+          className={`nav-links duration-500 md:static absolute bg-white md:min-h-fit min-h-[60vh] left-0 ${menuOpen ? 'top-[9%]' : 'top-[-100%]'
+            } md:w-auto w-full flex items-center px-5`}
+        >
+          <ul className="flex md:flex-row flex-col md:items-center md:gap-[4vw] gap-8">
+            <li>
+              <Link href="/projects" className="hover:text-gray-500">
+                PROJECTS
+              </Link>
+            </li>
+            <li>
+              <Link href="/about-us" className="hover:text-gray-500">
+                ABOUT US
+              </Link>
+            </li>
+            <li>
+              <Link href="/team" className="hover:text-gray-500">
+                TEAM
+              </Link>
+            </li>
+            <li>
+              <Link href="/contact" className="hover:text-gray-500">
+                CONTACT
+              </Link>
+            </li>
+          </ul>
+        </div>
+        <div className="flex items-center gap-6">
+        </div>
+      </nav>
+    </header>
+  );
+};
+
+export default Navbar;
